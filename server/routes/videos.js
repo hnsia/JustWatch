aad82f5702e6686cfe8391d33a73aeb45b66b4dd
hnsia <44498178+hnsia@users.noreply.github.com>
2023-07-14 07:07:01 +0800

    Implement like unlike routes

--- server/routes/videos.js
+++ server/routes/videos.js
@@ -2,11 +2,14 @@ import express from "express";
 import {
   addVideo,
   addView,
+  deleteVideo,
   getByTag,
+  getVideo,
   random,
   search,
   sub,
   trend,
+  updateVideo,
 } from "../controllers/video.js";
 import { verifyToken } from "../verifyToken.js";
 
@@ -14,9 +17,9 @@ const router = express.Router();
 
 // create a video
 router.post("/", verifyToken, addVideo);
-router.put("/:id", verifyToken, addVideo);
-router.delete("/:id", verifyToken, addVideo);
-router.get("/find/:id", addVideo);
+router.put("/:id", verifyToken, updateVideo);
+router.delete("/:id", verifyToken, deleteVideo);
+router.get("/find/:id", getVideo);
 router.put("/view/:id", addView);
 router.get("/trend", trend);
 router.get("/random", random);
